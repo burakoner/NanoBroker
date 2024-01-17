a8aa5dad3117d8e44387210170599fccff1e1a93
Burak Öner <info@burakoner.com>
2024-01-17 09:25:03 +0300

    Version 1.0.1

--- NanoBroker/Enums/BrokerEngine.cs
+++ NanoBroker/Enums/BrokerEngine.cs
@@ -1,8 +1,49 @@
 ﻿namespace NanoBroker;
 
-public enum BrokerEngine
+public enum BrokerEngine : byte
 {
-    RabbitMQ,
-    Kafka,
-    NATS,
+    /// <summary>
+    /// Apache Active MQ
+    /// </summary>
+    ActiveMQ = 1,
+
+    /// <summary>
+    /// Amazon MQ
+    /// </summary>
+    AmazonMQ = 2,
+
+    /// <summary>
+    /// Amazon Simple Queue Service
+    /// </summary>
+    AmazonSQS = 3,
+
+    /// <summary>
+    /// Azure Service Bus
+    /// </summary>
+    AzureServiceBus = 4,
+
+    /// <summary>
+    /// Google Pub-Sub
+    /// </summary>
+    GooglePubSub = 5,
+
+    /// <summary>
+    /// Kafka
+    /// </summary>
+    Kafka = 6,
+
+    /// <summary>
+    /// NATS
+    /// </summary>
+    NATS = 7,
+
+    /// <summary>
+    /// RabbitMQ
+    /// </summary>
+    RabbitMQ = 8,
+
+    /// <summary>
+    /// Redis
+    /// </summary>
+    Redis = 9,
 }
